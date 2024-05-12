f726b3ad8268fbdb49c028a6986f032ebad7b6a8
Jan Zadrobílek <ondracz97@gmail.com>
2024-05-12 15:31:28 +0200

    changes

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
+    extend: {
+      colors: {
+        'text': '#000000',
+        'background': '#ffffff',
+        'primary': '#707070',
+        'secondary': '#bfbfbf',
+        'accent': '#f2f2f2',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { filter: 'grayscale(100%)' },
+          '100%': { filter: 'grayscale(0%)' },
+        },
+      }, 
+      animation: {
+        fadeIn: 'fadeIn 0.7s ease-in',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+  ],
+}
