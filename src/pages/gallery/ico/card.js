5a42f2cf1a56b1e44fb9f59a109b4fc081563297
Jan Zadrobílek <ondracz97@gmail.com>
2024-05-15 09:55:37 +0200

    changes

--- src/pages/gallery/ico/card.js
+++ src/pages/gallery/ico/card.js
@@ -1,21 +1,46 @@
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next'
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback, useLayoutEffect } from 'react';
 
 function Card({ project }) {
     const { t } = useTranslation();
-    let name = (project.name.length > 25) ? project.name.substr(0, 23) + '...' : project.name;
+    const projectRef = useRef(null);
     const [image, setImage] = useState(null);
+    const [name, setName] = useState(null);
+
+    const handleResize = useCallback(() => {
+        if (projectRef.current) {
+            const maxLength = Math.floor((projectRef.current.getBoundingClientRect().width - 32) / 11);
+            const newName = project.name.length > maxLength ? project.name.slice(0, maxLength - 3) + '...' : project.name;
+            setName(newName);
+        }
+    }, [project.name]);
+
+    useLayoutEffect(() => {
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [handleResize]);
 
     useEffect(() => {
+        let isMounted = true;
+
         const loadImage = async () => {
             const imageModule = await import(`../${project.cover.src}`);
-            setImage(imageModule.default);
+            if (isMounted) {
+                setImage(imageModule.default);
+            }
         };
+
         loadImage();
+
+        return () => {
+            isMounted = false;
+        };
     }, [project.cover.src]);
     return (
         <Link to={`/project/${project.link}`}
+            ref={projectRef}
             className="flex flex-col flex-1 h-[22rem] rounded-xl shadow-md grayscale 
             focus:animate-fadeIn hover:animate-fadeIn hover:grayscale-0 hover:scale-[1.005]">
             {image && <img className="h-64 w-full object-cover rounded-xl" loading='lazy' src={image} alt="missing"></img>}
